77fd38c62e387000e1df64f4dc13973634f51815
Luis Román <luisromangz@gmail.com>
2014-01-21 11:27:55 +0100

    #93705: Improved WMSLayer declaration and docs.

--- src/layers/WMSLayer.js
+++ src/layers/WMSLayer.js
@@ -3,10 +3,21 @@
  * 
  * @class
  * @extends L.TileLayer.WMS
+ * @mixes SMC.layers.SingleLayer
  *
  * @author Luis Román (lroman@emergya.com)
  */
 SMC.layers.WMSLayer = L.TileLayer.WMS.extend(
 /** @lends SMC.layers.WMSLayer# */ {
 
+	includes: [SMC.layers.SingleLayer]
 });
+
+/**
+ * API factory method for ease creation of WMS layers.
+ * @params {String} url - The url the tiles are retrieved from
+ * @params {Object} options - Options for the layer.
+ */
+SMC.wmsLayer = function(url, options) {
+	return new SMC.layers.WMSLayer(url, options);
+};
