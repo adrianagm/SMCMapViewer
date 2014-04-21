a8113a892326aa0b9b70d1d9e0849aa71c492c83
Luis Román <luisromangz@gmail.com>
2014-04-21 17:38:47 +0200

    #93705: We use SMC.Util.deepClassInclude in classes that previously used just include and didn't work, or classes where includes had been discarded and we used indirection instead.

--- src/layers/markers/MarkerLayer.js
+++ src/layers/markers/MarkerLayer.js
@@ -13,23 +13,21 @@ require("../../../lib/LeafletHtmlIcon.js");
  * @mixes SMC.layers.SingleLayer
  */
 SMC.layers.markers.MarkerLayer = L.FeatureGroup.extend(
-	/** @lends SMC.layers.markers.MarkerLayer# */
-	{
-		includes: [SMC.layers.SingleLayer.prototype, SMC.layers.stylers.MarkerCssStyler.prototype],
+    /** @lends SMC.layers.markers.MarkerLayer# */
+    {
+        includes: SMC.Util.deepClassInclude([SMC.layers.SingleLayer, SMC.layers.stylers.MarkerCssStyler]),
 
-		STYLER: new SMC.layers.stylers.MarkerCssStyler(),
+        clusterGroup: new L.MarkerClusterGroup({
+            polygonOptions: {
+                fill: false,
+                stroke: false
+            }
+        }),
 
-		clusterGroup: new L.MarkerClusterGroup({
-			polygonOptions: {
-				fill: false,
-				stroke: false
-			}
-		}),
-
-		noClusterGroup: new L.FeatureGroup(),
+        noClusterGroup: new L.FeatureGroup(),
 
 
-		/*addTo: function(map) {
+        /*addTo: function(map) {
 			
 			L.LayerGroup.prototype.addTo.call(this, map);
 			if (map) {
@@ -39,134 +37,134 @@ SMC.layers.markers.MarkerLayer = L.FeatureGroup.extend(
 
 		},*/
 
-		removeLayer: function(layer) {
-			if (this.clusterGroup.hasLayer(layer)) {
-				this.clusterGroup.removeLayer(layer);
-			} else if (this.noClusterGroup.hasLayer(layer)) {
-				this.noClusterGroup.removeLayer(layer);
-			} else
-				this._map.removeLayer(layer);
-		},
+        removeLayer: function(layer) {
+            if (this.clusterGroup.hasLayer(layer)) {
+                this.clusterGroup.removeLayer(layer);
+            } else if (this.noClusterGroup.hasLayer(layer)) {
+                this.noClusterGroup.removeLayer(layer);
+            } else
+                this._map.removeLayer(layer);
+        },
 
 
-		onAdd: function(map) {
-			this.clusterGroup.addTo(map);
-			this.noClusterGroup.addTo(map);
-			L.LayerGroup.prototype.onAdd.call(this, map);
-			SMC.layers.SingleLayer.prototype.onAdd.call(this, map);
-			if (map) {
-				map.on("zoomend", this._onViewChanged, this);
-			}
-		},
+        onAdd: function(map) {
+            this.clusterGroup.addTo(map);
+            this.noClusterGroup.addTo(map);
+            L.LayerGroup.prototype.onAdd.call(this, map);
+            SMC.layers.SingleLayer.prototype.onAdd.call(this, map);
+            if (map) {
+                map.on("zoomend", this._onViewChanged, this);
+            }
+        },
 
-		addLayer: function(layer) {
+        addLayer: function(layer) {
 
-			if (layer instanceof L.Marker) {
-				var marker = layer;
-				this._applyStyles(marker);
+            if (layer instanceof L.Marker) {
+                var marker = layer;
+                this._applyStyles(marker);
 
-				marker.on("click", function() {
-					this.onFeatureClicked(marker);
-				}, this);
+                marker.on("click", function() {
+                    this.onFeatureClicked(marker);
+                }, this);
 
-				return marker;
+                return marker;
 
-			} else
-				throw new Error("This is not a marker");
+            } else
+                throw new Error("This is not a marker");
 
-		},
+        },
 
-		_sendFeatures: function(features){
-			var self = this;
-			$.each(features, function(index, feature){
-				self._addMarker(feature);
-			});
-		},
+        _sendFeatures: function(features) {
+            var self = this;
+            $.each(features, function(index, feature) {
+                self._addMarker(feature);
+            });
+        },
 
-		addMarkerFromFeature: function(features) {
-			if(L.Util.isArray(features) || typeof features == "object"){
-				this._sendFeatures(features);
-			}else{
-				this._sendFeatures(arguments);
-			}
-		},
+        addMarkerFromFeature: function(features) {
+            if (L.Util.isArray(features) || typeof features == "object") {
+                this._sendFeatures(features);
+            } else {
+                this._sendFeatures(arguments);
+            }
+        },
 
-		_addMarker: function(f) {
+        _addMarker: function(f) {
 
-			if(f.type != "Feature"){
-				return;
-			}
+            if (f.type != "Feature") {
+                return;
+            }
 
-			// For GeoJSON standar the first coordinate is the longitude
-			// Documentation http://geojson.org/geojson-spec.html#positions
-			var markerLocation = new L.LatLng(f.geometry.coordinates[1], f.geometry.coordinates[0]);
+            // For GeoJSON standar the first coordinate is the longitude
+            // Documentation http://geojson.org/geojson-spec.html#positions
+            var markerLocation = new L.LatLng(f.geometry.coordinates[1], f.geometry.coordinates[0]);
 
-			var marker = new L.Marker(markerLocation);
-			// We store this here so is avalaible later, on restylings because of zoom changes.
-			marker.properties = f.properties;
+            var marker = new L.Marker(markerLocation);
+            // We store this here so is avalaible later, on restylings because of zoom changes.
+            marker.properties = f.properties;
 
-			this.addLayer(marker);
-		},
+            this.addLayer(marker);
+        },
 
-		onFeatureClicked: function(feature) {
-			this.fireEvent("featureClick", feature);
-			//alert(feature.properties.name);
-		},
+        onFeatureClicked: function(feature) {
+            this.fireEvent("featureClick", feature);
+            //alert(feature.properties.name);
+        },
 
-		_applyStyles: function(marker, inCluster) {
-			if (!marker.properties) {
-				this.noClusterGroup.addLayer(marker);
-				return;
-			}
+        _applyStyles: function(marker, inCluster) {
+            if (!marker.properties) {
+                this.noClusterGroup.addLayer(marker);
+                return;
+            }
 
-			var zoom = this._map.getZoom();
-			var style = this.STYLER.applyStyle(marker.properties, zoom);
-			if (style.icon) {
-				marker.setIcon(style.icon);
-			}
+            var zoom = this._map.getZoom();
+            var style = this.applyStyle(marker.properties, zoom);
+            if (style.icon) {
+                marker.setIcon(style.icon);
+            }
 
-			if (inCluster) {
-				this.clusterGroup.removeLayer(marker);
-			} else {
-				this.noClusterGroup.removeLayer(marker);
-			}
-
-
-			if (style.disableClustering) {
-				this.noClusterGroup.addLayer(marker);
-			} else {
-				this.clusterGroup.addLayer(marker);
-			}
-
-			this.STYLER.addPopUp(marker, zoom);
-		},
-
-		_onViewChanged: function() {
-			var markersCluster = this.clusterGroup.getLayers();
-			var markersNoCluster = this.noClusterGroup.getLayers();
+            if (inCluster) {
+                this.clusterGroup.removeLayer(marker);
+            } else {
+                this.noClusterGroup.removeLayer(marker);
+            }
 
 
-			console.debug(this._map.getZoom());
+            if (style.disableClustering) {
+                this.noClusterGroup.addLayer(marker);
+            } else {
+                this.clusterGroup.addLayer(marker);
+            }
 
-			// Recorrer cluster
-			for (var i = 0; i < markersCluster.length; i++) {
-				var marker = markersCluster[i];
-				if (this.clusterGroup)
-					this._applyStyles(marker, true);
+            this.addPopUp(marker, zoom);
+        },
 
-			}
-
-			for (var i = 0; i < markersNoCluster.length; i++) {
-				var marker = markersNoCluster[i];
-				if (this.noClusterGroup)
-					this._applyStyles(marker, false);
-
-			}
+        _onViewChanged: function() {
+            var markersCluster = this.clusterGroup.getLayers();
+            var markersNoCluster = this.noClusterGroup.getLayers();
 
 
+            console.debug(this._map.getZoom());
 
-		}
+            // Recorrer cluster
+            for (var i = 0; i < markersCluster.length; i++) {
+                var marker = markersCluster[i];
+                if (this.clusterGroup)
+                    this._applyStyles(marker, true);
+
+            }
+
+            for (var i = 0; i < markersNoCluster.length; i++) {
+                var marker = markersNoCluster[i];
+                if (this.noClusterGroup)
+                    this._applyStyles(marker, false);
+
+            }
 
 
 
-	});
+        }
+
+
+
+    });
--- src/providers/WFSProvider.js
+++ src/providers/WFSProvider.js
@@ -9,126 +9,126 @@ require("./URLFeatureProvider.js");
  */
 SMC.providers.WFSProvider = SMC.providers.URLFeatureProvider.extend({
 
-	options: {
-		/**
-		 * WFS Service.
-		 * @property {string} service - The default wfs service.
-		 */
-		service: "wfs",
-		/**
-		 * WFS Version.
-		 * @property {string} version - The default wfs version.
-		 */
-		version: "1.1.0",
-		/**
-		 * WFS request.
-		 * @property {string} request - The default wfs request.
-		 */
-		request: "GetFeature",
-		/**
-		 * WFS Type name.
-		 * @property {string} typeName - The default wfs typename.
-		 */
-		typeName: "namespace:featuretype",
-		/**
-		 * WFS Feature identifier.
-		 * @property {string} featureID - The default wfs feature id.
-		 */
-		featureID: null,
-		/**
-		 * WFS count parameter.
-		 * @property {string} count - The default wfs count parameter.
-		 */
-		count: null,
-		/**
-		 * WFS maxFeatures parameter.
-		 * @property {string} maxFeatures - The default wfs max features parameter.
-		 */
-		maxFeatures: null,
-		/**
-		 * WFS sortBy parameter.
-		 * @property {string} sortBy - The default wfs sort by parameter.
-		 */
-		sortBy: null,
-		/**
-		 * WFS propertyName parameter.
-		 * @property {string} propertyName - The default wfs property nanem parameter.
-		 */
-		propertyName: null,
-		/**
-		 * WFS bbox parameter.
-		 * @property {string} bbox - The default wfs bounding box parameter.
-		 */
-		bbox: null,
-		/**
-		 * WFS srsName parameter.
-		 * @property {string} srsName - The default wfs coordinate reference system parameter.
-		 */
-		srsName: "EPSG:4326",
-		/**
-		 * WFS CQL Filter parameter.
-		 * @property {string} cqlFilter - The default wfs cql filter parameter.
-		 */
-		cqlFilter: null,
-		/**
-		 * WFS outputFormat parameter.
-		 * @property {string} outputFormat - The default wfs output format parameter.
-		 */
-		outputFormat: "text/javascript",
-		/**
-		 * WFS serverURL parameter.
-		 * @property {string} serverURL - The wfs server url path parameter.
-		 */
-		serverURL: null,
-		/**
-		 * WFS format_options parameter.
-		 * @property {string} format_options - The wfs format options parameter.
-		 */
-		format_options: "callback:geojsonWFS"
-	},
-	/**
-	 * Initialize the object with the params
-	 * @param {object} options - object with need parameters
-	 */
-	initialize: function(options){
-		L.Util.setOptions(this, options);
-	},
-	/**
-	 * Send WFS request to get the features
-	 * @returns {object} Deferred object from jQuery
-	 */
-	doFeaturesLoading: function() {
-		if(this.options.serverURL != null){
-			return $.ajax({
-				url: this.options.serverURL,
-				data: this.getParamsFromOptions(),
-				jsonpCallback: "geojsonWFS",
-				type: "GET",
-				dataType: "jsonp",
-				jsonp: false
-			});
-		}
-		return $.Deferred();
-	},
+    options: {
+        /**
+         * WFS Service.
+         * @property {string} service - The default wfs service.
+         */
+        service: "wfs",
+        /**
+         * WFS Version.
+         * @property {string} version - The default wfs version.
+         */
+        version: "1.1.0",
+        /**
+         * WFS request.
+         * @property {string} request - The default wfs request.
+         */
+        request: "GetFeature",
+        /**
+         * WFS Type name.
+         * @property {string} typeName - The default wfs typename.
+         */
+        typeName: "namespace:featuretype",
+        /**
+         * WFS Feature identifier.
+         * @property {string} featureID - The default wfs feature id.
+         */
+        featureID: null,
+        /**
+         * WFS count parameter.
+         * @property {string} count - The default wfs count parameter.
+         */
+        count: null,
+        /**
+         * WFS maxFeatures parameter.
+         * @property {string} maxFeatures - The default wfs max features parameter.
+         */
+        maxFeatures: null,
+        /**
+         * WFS sortBy parameter.
+         * @property {string} sortBy - The default wfs sort by parameter.
+         */
+        sortBy: null,
+        /**
+         * WFS propertyName parameter.
+         * @property {string} propertyName - The default wfs property nanem parameter.
+         */
+        propertyName: null,
+        /**
+         * WFS bbox parameter.
+         * @property {string} bbox - The default wfs bounding box parameter.
+         */
+        bbox: null,
+        /**
+         * WFS srsName parameter.
+         * @property {string} srsName - The default wfs coordinate reference system parameter.
+         */
+        srsName: "EPSG:4326",
+        /**
+         * WFS CQL Filter parameter.
+         * @property {string} cqlFilter - The default wfs cql filter parameter.
+         */
+        cqlFilter: null,
+        /**
+         * WFS outputFormat parameter.
+         * @property {string} outputFormat - The default wfs output format parameter.
+         */
+        outputFormat: "text/javascript",
+        /**
+         * WFS serverURL parameter.
+         * @property {string} serverURL - The wfs server url path parameter.
+         */
+        serverURL: null,
+        /**
+         * WFS format_options parameter.
+         * @property {string} format_options - The wfs format options parameter.
+         */
+        format_options: "callback:geojsonWFS"
+    },
+    /**
+     * Initialize the object with the params
+     * @param {object} options - object with need parameters
+     */
+    initialize: function(options) {
+        L.Util.setOptions(this, options);
+    },
+    /**
+     * Send WFS request to get the features
+     * @returns {object} Deferred object from jQuery
+     */
+    doFeaturesLoading: function() {
+        if (this.options.serverURL !== null) {
+            return $.ajax({
+                url: this.options.serverURL,
+                data: this.getParamsFromOptions(),
+                jsonpCallback: "geojsonWFS",
+                type: "GET",
+                dataType: "jsonp",
+                jsonp: false
+            });
+        }
+        return $.Deferred();
+    },
 
-	/**
-	 * Get params from options attributes
-	 * @returns {object} Object with the wfs params to send
-	 */
-	getParamsFromOptions: function(){
-		var params = {};
-		for(option in this.options){
-			if(this.options[option] != null){
-				params[option] = this.options[option];
-			}
-		}
-		return params;
-	}
+    /**
+     * Get params from options attributes
+     * @returns {object} Object with the wfs params to send
+     */
+    getParamsFromOptions: function() {
+        var params = {};
+        for (var option in this.options) {
+            if (this.options[option] !== null) {
+                params[option] = this.options[option];
+            }
+        }
+        return params;
+    }
 });
 /**
  * API factory method for ease creation of wfs features providers.
  * @params {Object} options - Options for wfs the provider.
  */
 SMC.wfsProvider = function(options) {
-	return new SMC.providers.WFSProvider(options);
+    return new SMC.providers.WFSProvider(options);
 };
