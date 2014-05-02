2069eca7280098c12fe5c0c534e20ab350f783dd
Luis Román <luisromangz@gmail.com>
2014-05-02 12:12:46 +0200

    #93745: Changes due package changes.

--- build/api-deps.js
+++ build/api-deps.js
@@ -2,10 +2,10 @@ require("../src/layers/WMSLayer.js");
 require("../src/layers/TileLayer.js");
 require("../src/layers/markers/MarkerLayer.js");
 require("../src/layers/geometry/GeometryLayer.js");
+require("../src/layers/Folder.js");
 
 require("../src/providers/WFSProvider.js");
 require("../src/layers/markers/WFSMarkerLayer.js");
 
-require("../src/layerTree/LayerTreeLeaf.js");
-require("../src/layerTree/LayerTreeFolder.js");
-require("../src/layerTree/Folder.js");
+require("../src/controls/layerTree/LayerTreeLeaf.js");
+require("../src/controls/layerTree/LayerTreeFolder.js");
